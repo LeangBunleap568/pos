7c20d641e6888355bbc3c64842523dfe11e8b3c4
Leang Bunleap <leangbunleap007@gmail.com>
2026-03-23 14:37:19 +0700

    fix

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,5 @@
 const user = require("../models/User")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const logError = require("../service/service")
 const { isValid } = require("../service/prevent")
