2e433d28e90aab527d88f1a739bc8ffbbc81dde9
Leang Bunleap <leangbunleap007@gmail.com>
2026-03-28 17:06:56 +0700

    hi

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,24 +1,16 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
 const sequelize = new Sequelize(
-    process.env.DB_NAME || 'project',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASSWORD || '',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 3306,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 13721,
         dialect: 'mysql',
-        logging: false,
-        define: {
-            timestamps: false
-        },
         dialectOptions: {
             ssl: {
+                require: true,
                 rejectUnauthorized: false
             }
         }
     }
 );
-
-module.exports = sequelize;
